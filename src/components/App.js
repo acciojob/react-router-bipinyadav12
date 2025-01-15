adbb3e18a0ec643ab0fb8a1e5186ad538cb23c67
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2025-01-15 08:36:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,28 @@
-import './../styles/App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router';
-function Home() {
-  return (
-    <div>
-      <h1>Welcome to my website!</h1>
-      
-    </div>
-  );
-}
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
-function About() {
-  return (
-    <div>
-      <h1>About</h1>
-      <p>This is simple React Router program.</p>
-    </div>
-  );
-}
-
-
-function App() {
+const App = () => {
   return (
     <Router>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </Router>
   );
-}
-export default App
+};
 
+export default App;
